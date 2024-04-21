ffabdcfd1172f2fec7429196e62235e2a88a5e3f
computer-note <planomenes@gmail.com>
2024-04-21 20:48:53 +0900

    LV-0-001. 문자열 출력하기

--- level-0/001-문자열_출력하기.js
+++ level-0/001-문자열_출력하기.js
@@ -1,13 +1,35 @@
 const readline = require('readline');
+
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
 let input = [];
 
 rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
+	input.push(line);
+}).on('close', function () {
+	let str = input[0];
+	console.log(str);
 });
+
+/*****************
+  다른 사람의 풀이
+******************/
+function otherSolution1() {
+	//rl변수를 선언하지 않고 createInterface반환값에 바로 이벤트헨들러 등록하는 방법
+	readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	}).on('line', function (line) { })
+		.on('close', function () { });
+}
+
+function otherSolution2() {
+	//이벤트헨들러의 콜백함수에 console.log 함수를 바로 등록하는 방법
+	readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	}).on('line', console.log);
+}
