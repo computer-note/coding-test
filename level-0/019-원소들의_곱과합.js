26ad3f0a6e8e1a687a1265e483b0bcc302778cb9
computer-note <planomenes@gmail.com>
2024-04-23 09:17:51 +0900

    LV-0-019. 원소들의 곱과 합

---
+++ level-0/019-원소들의_곱과합.js
@@ -0,0 +1,27 @@
+mySolution([1,2,3]);
+
+function mySolution(num_list) {
+	const allElemProduct = num_list.reduce((acc, cur) => acc *= cur);
+	const allElemSumSquare = num_list.reduce((acc, cur) =>
+		acc += cur) ** 2;
+
+	return allElemProduct < allElemSumSquare ? 1 : 0;
+}
+
+/* 반복적으로 하는 실수
+	const allElemSumSquare = num_list.reduce((acc, cur) 
+	   => acc += cur) ** 2; 
+		//SyntaxError: Unexpected token '=>'
+		//ts(1200) line terminator not permitted before arrow
+*/
+
+/* 문제
+정수가 담긴 리스트 num_list가 주어질 때, 
+  모든 원소들의 곱이 
+  모든 원소들의 합의 제곱보다 
+	 작으면 1을 
+	 크면 0을 
+return하도록 solution 함수를 완성해주세요.
+
+https://school.programmers.co.kr/learn/courses/30/lessons/181929
+*/
