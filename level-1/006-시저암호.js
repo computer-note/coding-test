11b6f3548bb58bc5e92b191c4627f2bd9f9a0a0d
computer-note <planomenes@gmail.com>
2024-05-02 16:28:56 +0900

    LV-1-006. 시저암호

---
+++ level-1/006-시저암호.js
@@ -0,0 +1,52 @@
+const result = solution("abcdxyzXYZ", 1);
+console.log(result);
+
+function solution(s, n) {
+	const encodedCharacters = [...s].map(c =>
+		c === ' ' ? c : caesarEncode(c, n)
+	);
+
+	return encodedCharacters.join('');
+}
+
+function caesarEncode(char, offset) {
+	let caseBaseCharCode =
+		(char === char.toUpperCase() ?
+			'A' : 'a')
+			.codePointAt(0);
+
+	//map to range [0, 25]
+	let normalizedCharCode =
+		char.codePointAt(0) - caseBaseCharCode;
+
+	//caesar encode
+	normalizedCharCode =
+		(normalizedCharCode + offset) % 26;
+
+	//recover case 
+	return String.fromCodePoint(
+		normalizedCharCode + caseBaseCharCode);
+}
+
+/* 처음 실수한 부분
+	//map to range [1, 26]
+	let normalizedCharCode =
+		char.codePointAt(0) - caseBaseCharCode + 1;
+
+	//caesar encode
+	normalizedCharCode =
+		(normalizedCharCode + offset) % 26;
+
+	//recover case 
+	return String.fromCodePoint(
+		normalizedCharCode + caseBaseCharCode - 1);
+*/
+
+/* 문제: 시저 암호
+어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는  암호화 방식을 시저 암호라고 합니다. 
+예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. 
+"z"는 1만큼 밀면 "a"가 됩니다. 
+문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12926
+*/
